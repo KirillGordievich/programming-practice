aabdd70832e99bce7eb62a92301f5ce25044b368
Kirill Gordievich <kirill.gordievich777@gmail.com>
2024-10-28 15:36:37 +0300

    add circle solution for 4 problem

--- yandex/preparation_for_interview/4/main.py
+++ yandex/preparation_for_interview/4/main.py
@@ -1,7 +1,7 @@
 import sys
 
 
-def generate_brackets(n: int):
+def generate_brackets_recursive(n: int):
     result = []
     generate_brackets_iter(result, "", 0, 0, n)
     return result
@@ -19,10 +19,34 @@ def generate_brackets_iter(result: list[str], current: str, open_left: int, clos
         generate_brackets_iter(result, current + ")", open_left, close_right + 1, max_seq)
 
 
+def generate_brackets_circle(n: int):
+    result = []
+    stack = [("", 0, 0)]  # (current seq, open count, close count)
+
+    while stack:
+        current, open_left, close_right = stack.pop()
+
+        if close_right == n:
+            result.append(current)
+            continue
+
+        if open_left < n:
+            stack.append((current + "(", open_left + 1, close_right))
+
+        if close_right < open_left:
+            stack.append((current + ")", open_left, close_right + 1))
+
+    return result[::-1]
+
+
 def main():
     count = int(sys.stdin.readline())
 
-    print(generate_brackets(count))
+    recursive_result = generate_brackets_recursive(count)
+    circle_result = generate_brackets_circle(count)
+
+    print(recursive_result == circle_result)
+    print(recursive_result, circle_result)
 
 
 if __name__ == "__main__":
