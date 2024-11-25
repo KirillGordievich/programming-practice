afc5923032c9ee4746a5d1d8d567b57a546f1843
Kirill Gordievich <kirill.gordievich777@gmail.com>
2024-11-25 19:30:14 +0300

    add arrays

---
+++ golang/syntax/arrays.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numbers [5]int = [5]int{1, 2, 3, 4}
+	fmt.Println(numbers)
+
+	fmt.Println(numbers[0]) // 1
+	fmt.Println(numbers[4]) // 0
+
+	numbers[0] = 87
+	fmt.Println(numbers) // 87
+
+	// Если в квадратных скобках вместо длины указано троеточие, то длина массива определяется, исходя из количества переданных ему элементов:
+	var numbers2 = [...]int{1, 2, 3, 4, 5} // длина массива 5
+	fmt.Println(numbers2)                  // [1 2 3]
+
+	// Также можно применять сокращенное определение переменной массива:
+	numbers3 := [...]int{4, 4, 1, 2, 0} // длина массива 5
+	fmt.Println(numbers3)               // [1 2 3]
+}
