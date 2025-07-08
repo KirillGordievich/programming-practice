0f04b7d70b2260ee7cf731aa6377e09e252400e6
Kirill Gordievich <kirill.gordievich777@gmail.com>
2025-07-08 20:46:13 +0300

    update sync_async_decorator

--- python/yandex/preparation_for_interview/interview_tasks/decorator_sync_async.py
+++ python/yandex/preparation_for_interview/interview_tasks/decorator_sync_async.py
@@ -28,12 +28,16 @@ def sync_async_decorator(func):
 # Example usage with a synchronous function
 @sync_async_decorator
 def my_sync_function():
+    """ I love this sync function so much """
+
     print("Inside synchronous function")
     return "Sync Result"
 
 # Example usage with an asynchronous function
 @sync_async_decorator
 async def my_async_function():
+    """ I love this async function so much """
+
     print("Inside asynchronous function")
     await asyncio.sleep(0.1)  # Simulate some async work
     return "Async Result"
@@ -41,11 +45,11 @@ async def my_async_function():
 if __name__ == "__main__":
     # Call the synchronous function
     sync_result = my_sync_function()
-    print(f"Returned: {sync_result}\n")
+    print(f"Returned: {sync_result} from {my_sync_function.__name__} / {my_sync_function.__doc__}")
 
     # Call the asynchronous function
     async def main():
         async_result = await my_async_function()
-        print(f"Returned: {async_result}")
+        print(f"Returned: {async_result} from {my_async_function.__name__} / {my_async_function.__doc__}")
 
     asyncio.run(main())
